64dfe700d005ebbf89c984142c24d370f9dc0602
Austinb96 <austin.m.babcock@gmaIl.com>
2023-01-19 15:53:56 -0600

    First Commit. Everything works. dbf file will upload to google sheet

---
+++ gsheet.py
@@ -0,0 +1,29 @@
+﻿import pandas as pd
+from dbfread import DBF
+import gspread
+from gspread_dataframe import set_with_dataframe
+
+# File for Importing
+filePath = ''
+# Sheet Key
+key = ""
+# Worksheet you are working on
+worksheetName = ""
+# Columns to keep
+columnsToKeep = ['']
+
+sa = gspread.service_account(filename='service_account.json')
+
+# Take dbf and make it into DataFrame
+dbf = DBF(filePath)
+df = pd.DataFrame(iter(dbf))
+
+# Make new DataFrame with only specified Columns
+df = df[columnsToKeep]
+
+sheet = sa.open_by_key(key)
+worksheet = sheet.worksheet(worksheetName)
+
+worksheet.clear()
+set_with_dataframe(worksheet, df)
+
